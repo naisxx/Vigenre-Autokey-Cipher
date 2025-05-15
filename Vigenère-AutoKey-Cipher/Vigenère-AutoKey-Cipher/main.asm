d2af05acf40d99ffa03f8933b846b2a03d9ed3f9
onaisahmed <onaisahmed02@gmail.com>
2025-05-15 20:37:49 +0500

    Added basic prompts and input reading for keyword and plaintext

--- Vigenère-AutoKey-Cipher/Vigenère-AutoKey-Cipher/main.asm
+++ Vigenère-AutoKey-Cipher/Vigenère-AutoKey-Cipher/main.asm
@@ -1,13 +1,31 @@
 include Irvine32.inc
 
 .data
-msg BYTE "Hello World!", 0
+	promptPlainText	 BYTE "Enter plaintext: ", 0
+	promptKeyword	 BYTE "Enter keyword: ", 0
+
+	maxLength		 = 100
+	plainText		 BYTE maxLength DUP(?)
+	keyword			 BYTE maxLength DUP(?)
 
 .code
 main PROC
-    mov edx, OFFSET msg
-    call WriteString
-    call Crlf
-    exit
+	; Reading PlainText
+	mov edx, OFFSET promptPlainText
+	call WriteString
+
+	mov edx, OFFSET plainText
+	mov ecx, maxLength
+	call ReadString
+
+	; Reading Keyword
+	mov edx, OFFSET promptKeyword
+	call WriteString
+
+	mov edx, OFFSET keyword
+	mov ecx, maxLength
+	call ReadString
+
+	exit
 main ENDP
 END main
