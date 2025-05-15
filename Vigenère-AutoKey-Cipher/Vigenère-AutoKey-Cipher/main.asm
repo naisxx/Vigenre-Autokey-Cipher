a93cb84a34e488810c374d3ecf3fb420177e874c
onaisahmed <onaisahmed02@gmail.com>
2025-05-16 01:52:52 +0500

    Added code to convert plaintext and keyword to uppercase

--- Vigenère-AutoKey-Cipher/Vigenère-AutoKey-Cipher/main.asm
+++ Vigenère-AutoKey-Cipher/Vigenère-AutoKey-Cipher/main.asm
@@ -8,6 +8,9 @@ include Irvine32.inc
 	plainText		 BYTE maxLength DUP(?)
 	keyword			 BYTE maxLength DUP(?)
 
+	debugPlainTextMsg   BYTE "[DEBUG] Uppercase Plaintext: ", 0
+    debugKeywordMsg     BYTE "[DEBUG] Uppercase Keyword: ", 0
+
 .code
 main PROC
 	; Reading PlainText
@@ -25,6 +28,68 @@ main PROC
 	mov edx, OFFSET keyword
 	mov ecx, maxLength
 	call ReadString
+
+	; Converting PLAINTEXT to uppercase
+	mov esi, OFFSET plainText
+	convertPlainTextLoop:
+		; Checking if it's the end of the string
+		mov al, [esi]
+		cmp al, 0
+		je convertKeyword
+
+		; Checking if it's in lowercase range i.e 0-25
+		cmp al, 'a'
+		jb skipConvert
+		cmp al, 'z'
+		ja skipConvert
+
+		; Converting to Uppercase
+		sub al, 32
+		mov [esi], al
+	
+	skipConvert:
+		inc esi
+		jmp convertPlainTextLoop
+
+	convertKeyword:
+		mov esi, OFFSET keyword
+
+	convertKeywordLoop:
+		 mov al, [esi]
+		cmp al, 0
+		je doneConvert
+
+		cmp al, 'a'
+		jb skipKeyConvert
+		cmp al, 'z'
+		ja skipKeyConvert
+
+		sub al, 32
+		mov [esi], al
+
+	skipKeyConvert:
+		inc esi
+		jmp convertKeywordLoop
+
+	doneConvert:
+
+	printDebug:
+    call Crlf
+    mov edx, OFFSET debugPlainTextMsg
+    call WriteString
+
+    mov edx, OFFSET plainText
+    call WriteString
+
+    call Crlf
+    mov edx, OFFSET debugKeywordMsg
+    call WriteString
+
+    mov edx, OFFSET keyword
+    call WriteString
+    call Crlf
+
+
 
 	exit
 main ENDP
